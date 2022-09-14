89d5b88d469a7d771f7da7114115c8ee2f4f29db
AlvinZheng11 <azheng@cub.uca.edu>
2022-09-14 14:21:26 -0500

    Added Algorithim
    added euclid's algorithm

--- TestPi.py
+++ TestPi.py
@@ -2,6 +2,31 @@
 """
 Created on Wed Sep 14 11:15:52 2022
 
-@author: landon
+@author: landon, Alvin
 """
-print ('hello world')
+import math
+
+
+#Euclid's Algorithim
+#Public Key Generation
+def pubKeyGen(p,q):
+   n = p*q
+   e = 0
+   phi = (p-1)*(q-1)
+
+   for x in range(phi):
+       if(math.gcd(x,phi) == 1 and x > 1):
+          e = x
+          break
+   return(e,n,phi)
+
+
+
+def main():
+    p = 1
+    q = 1
+    n = 1
+    e = 1
+    phi = 1
+    d = 1
+    
